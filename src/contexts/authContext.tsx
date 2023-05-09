a834a9a35d7eb69015766b469116eeccb3495c96
Sahit <sahit5s18@gmail.com>
2023-05-09 17:49:37 +0200

    Load User dependency fixed

--- src/contexts/authContext.tsx
+++ src/contexts/authContext.tsx
@@ -59,7 +59,7 @@ export const AuthContextProvider: FC<IAuthContextProviderProps> = ({ children })
 // eslint-disable-next-line react-hooks/exhaustive-deps
 useEffect(() => {
   loadUser();
-}, [accessToken]);
+}, [accessToken,loadUser]);
 
   useEffect(() => {
     if (accessToken) {
